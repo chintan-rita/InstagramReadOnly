db408197889660d7c9c5777fe41af14e6ea6684d
Saurabh Shah <sshah1@L-SNVR0NPFFT-M.local>
2015-09-10 17:18:35 -0700

    pulling json

--- InstagramReadOnly/InstagramReadOnly/PhotosViewController.swift
+++ InstagramReadOnly/InstagramReadOnly/PhotosViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,15 +18,25 @@ class PhotosViewController: UIViewController {
     }
     
     func makeNetworkRequest() {
-        var clientId = "46e4a03f28954aed8d2bc587dd698124"
+        let clientId = "46e4a03f28954aed8d2bc587dd698124"
         
-        var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
-        var request = NSURLRequest(URL: url)
+        let url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        let request = NSURLRequest(URL: url)
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response,data,err) -> Void in
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as! NSDictionary
+        
+        
+        let photos = responseDictionary["data"] as! NSArray
+        NSLog("response: \(photos)")
+
             
+
+//            self.tableView.reloadData()
             
+
         }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
